b61c9f87fe78f8208eac08cebd213bebbefc2810
denny.k <denny.k@kakaocorp.com>
2020-08-25 23:57:16 +0900

    batch, filter vc 파일 생성

---
+++ KB-iOS/ViewControllers/BatchSearchViewController.swift
@@ -0,0 +1,31 @@
+//
+//  BatchSearchViewController.swift
+//  KB-iOS
+//
+//  Created by Denny on 2020/08/25.
+//  Copyright © 2020 KakaoTalk. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class BatchSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
